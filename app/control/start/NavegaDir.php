9dd6a02859b43700410c486ca22ed629128be4df
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-09-08 00:37:41 -0300

    add: NavegaDIR 0.5

--- app/control/start/NavegaDir.php
+++ app/control/start/NavegaDir.php
@@ -6,8 +6,38 @@ class NavegaDir extends TPage
     public function __construct()
     {
         parent::__construct();
-        echo '<br> Voce está no Adianti Framework puro' . "<br>";
-        echo '<br> Aqui será construído o programa NavegaDir';
+        echo '<br> Aqui será construído o programa NavegaDir<br> <br>';
+
+        $directory = "./";
+        $scan = scandir($directory);
+
+        sort($scan);
+
+        $formattedScan = [];
+        foreach ($scan as $scaned) {
+            $fullPath = $directory . '/' . $scaned;
+            
+            $formattedEntries[] = [
+            'name' => $scaned,
+            'fullPath' => $fullPath,
+            'isDirectory' => is_dir($fullPath),
+            ];
+            
+        }
+
+        //Display
+        echo '<ul>';
+        foreach ($formattedEntries as $entry){
+            if (!$entry["isDirectory"]){
+                echo "<li>" . $entry['name'] . "</li>";
+            };
+        };
+        echo '</ul>';
+
+
+
+        echo "\n";
+
     }
     
 }
